88cf0474e816098eb76ecbf65d6a4d45028848ae
武世鹏 <18234097765@163.com>
2020-06-03 15:21:26 +0800

    初始化，隐藏，流程控制初步完成

---
+++ src/components/GenerateForm.vue
@@ -0,0 +1,304 @@
+<template>
+  <div>
+    <el-form
+      ref="generateForm"
+      label-suffix=":"
+      :size="data.config.size"
+      :model="models"
+      :rules="rules"
+      :label-position="data.config.labelPosition"
+      :label-width="data.config.labelWidth + 'px'"
+    >
+      <template v-for="item in data.list">
+        <template v-if="item.type == 'grid'">
+          <el-row
+            :key="item.key"
+            type="flex"
+            :gutter="item.options.gutter ? item.options.gutter : 0"
+            :justify="item.options.justify"
+            :align="item.options.align"
+          >
+            <el-col v-for="(col, colIndex) in item.columns" :key="colIndex" :span="col.span">
+              <template v-for="citem in col.list">
+                <el-form-item
+                  v-if="citem.type=='blank'"
+                  :label="citem.name"
+                  :prop="citem.model"
+                  :key="citem.key"
+                >
+                  <slot :name="citem.model" :model="models"></slot>
+                </el-form-item>
+                <genetate-form-item
+                  v-else
+                  :key="citem.key"
+                  :models.sync="models"
+                  :remote="remote"
+                  :rules="rules"
+                  :widget="citem"
+                  @input-change="onInputChange"
+                ></genetate-form-item>
+              </template>
+            </el-col>
+          </el-row>
+        </template>
+
+        <template v-else-if="item.type == 'blank'">
+          <el-form-item :label="item.name" :prop="item.model" :key="item.key">
+            <slot :name="item.model" :model="models"></slot>
+          </el-form-item>
+        </template>
+
+        <template v-else>
+          <genetate-form-item
+            :key="item.key"
+            :models.sync="models"
+            :rules="rules"
+            :widget="item"
+            @input-change="onInputChange"
+            @el-change="onElChange"
+            :remote="remote"
+            :ref="item.model"
+            v-show="!item.options.hidden"
+            :class="item.options.customClass"
+          ></genetate-form-item>
+        </template>
+      </template>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import GenetateFormItem from "./GenerateFormItem";
+import { loadJs } from "../util/index.js";
+import getFee from '../util/fee.js'; //模拟流控引擎数据
+const FEE = new getFee();
+export default {
+  name: "fm-generate-form",
+  components: {
+    GenetateFormItem
+  },
+    /** 
+     * data为表单渲染原始数据 
+     * remote为获取服务端数据的回调函数 
+     * value为覆盖表单原始默认值的数据
+     * dyData为公共区动态数据
+    */
+  props: ["data", "remote", "value", "insite", "dyData"], 
+  data() {
+    return {
+      models: {}, // form表单对象所有组件key value组成的json
+      rules: {}, // form表单对象所有组件对应校验规则
+      hideObj: {}, // 后期添加非form making自有属性，用于存储表单隐藏条件的字段
+      haveHide: false, // 后期添加非form making自有属性，是否触发过节点隐藏
+      focusItems: [], // 后期添加非form making自有属性，可聚焦组件节点集合
+      focusIndex: 0 // 后期添加非form making自有属性，当前可聚焦节点下标
+    };
+  },
+  created() {
+    this.generateModle(this.data.list);
+  },
+  mounted() {
+    for(let i=0;i<this.data.list.length;i++){
+      this.hideData(this.data.list[i],i)
+      this.getFocus(this.data.list[i]);
+    }
+    this.focusItems = document.querySelectorAll(".canFocus input");
+    this.handelFocus()  
+  },
+  methods: {
+    // 动态数据处理函数
+    dynamicData(targetdata){
+      // 动态数据赋值
+      // let pdf = this.dyData.platform;
+      // let user = this.dyData.user;
+      var pdf = FEE.flow.platform;
+      var user = FEE.flow.user;
+      if(targetdata.options.defaultValue!="" && (targetdata.options.defaultValue)[0]=="@"){
+          var temp = targetdata.options.defaultValue.substring(1);
+          var tempJson = eval(temp)
+          if(tempJson!=""||tempJson!=null){
+            targetdata.options.defaultValue = tempJson
+          }
+        }
+    },
+    // 生成models、rules对象
+    generateModle(genList) {
+      for (let i = 0; i < genList.length; i++) {
+        //  FIXME 设置响应式属性 hideObj
+
+        if(genList[i].hidden && genList[i].hidden !== ""){
+          this.$set(
+            this.hideObj,
+            genList[i].model,
+            genList[i].options.defaultValue || ""
+          );
+        }
+        // FIXME 动态数据处理
+        this.dynamicData(genList[i])
+        
+        // 流控引擎数据处理(数据暂时没有与组件绑定的，先不处理)
+
+        if (genList[i].type === "grid") {
+          genList[i].columns.forEach(item => {
+            this.generateModle(item.list);
+          });
+        } else {
+          // 处理非表格类型的组件
+          if (
+            // 如果value对象不为空且具有与当前组件model同名的属性，则将值赋给models的该属性（这样就修改了组件原始值）
+            this.value &&
+            Object.keys(this.value).indexOf(genList[i].model) >= 0
+          ) {
+            this.models[genList[i].model] = this.value[genList[i].model];
+          } else {
+            // 如果value为空判断当前组件的类型是否为空类型
+            if (genList[i].type === "blank") {
+              console.log("g2");
+              // 如果为空类型则为models添加对应的响应式属性，并赋值为默认值
+              this.$set(
+                this.models,
+                genList[i].model,
+                genList[i].options.defaultType === "String" // 如果默认类型为字符串类型赋值为空字符串
+                  ? ""
+                  : genList[i].options.defaultType === "Object" //如果不是字符串类型判断是否是对象类型，如果是赋值空对象，否则赋值空数组
+                  ? {}
+                  : []
+              );
+            } else {
+              // 如果value为空并且组件为非空类型组件将组件默认值赋值给models对应属性
+              // 这个if条件为后期加入的，判断是否已触发过隐藏条件
+              if (!this.haveHide) {
+                this.models[genList[i].model] = genList[i].options.defaultValue;
+              }
+
+              // 组件创建完成后models为组件 key:value 键值对json
+            }
+          }
+          // 如果rules对象存在当前组件的校验规则
+          if (this.rules[genList[i].model]) {
+            // 执行此段代码后rules的每个属性为组件默认校验规则和传入检验规则组成的数组
+            this.rules[genList[i].model] = [
+              ...this.rules[genList[i].model], // 将当前数组值展开
+              ...genList[i].rules.map(item => {
+                // 展开当前组件的校验规则数组，遍历数组每一项（为json）
+                if (item.pattern) {
+                  // 如果存在 pattern属性则返回一个新对象，该对象包含此条校验规则的所有属性以及pattern属性，值为item.pattern的返回值
+                  return { ...item, pattern: eval(item.pattern) };
+                } else {
+                  // 不存在pattern属性则返回所有item组成的新对象
+                  return { ...item };
+                }
+              })
+            ];
+          } else {
+            // 如果rules对象不存在当前组件的校验规则则展开传入的校验规则
+            this.rules[genList[i].model] = [
+              ...genList[i].rules.map(item => {
+                if (item.pattern) {
+                  return { ...item, pattern: eval(item.pattern) };
+                } else {
+                  return { ...item };
+                }
+              })
+            ];
+          }
+        }
+      }
+    },
+    // 验证并获取输入框当前的值
+    getData() {
+      return new Promise((resolve, reject) => {
+        // 执行form表单验证函数
+        this.$refs.generateForm.validate(valid => {
+          if (valid) {
+            // 逐条验证当前组件的校验规则
+            resolve(this.models);
+          } else {
+            reject(new Error(this.$t("fm.message.validError")).message);
+          }
+        });
+      });
+    },
+    // 重置表单
+    reset() {
+      this.$refs.generateForm.resetFields();
+    },
+    // 数据改变时遍历数据判断隐藏条件
+    hideData(activedata,activeIndex) {
+      //判断显示隐藏
+      if (activedata.hidden && activedata.hidden !== "") {
+        if (eval(activedata.hidden)) {
+          this.haveHide = true;
+          activedata.options.hidden = true;
+          // 隐藏节点时获取到被隐藏节点input，从聚焦节点列表中删除
+          var removeParent = this.$refs[activedata.model][0].$el;
+          removeParent.classList.add("remove");
+          var remove = document.querySelector(".remove input");
+          [...this.focusItems].splice(activeIndex,1)
+          console.log(this.$refs[activedata.options.model])
+        } else if (this.hideIndex && !eval(activedata.hidden)) {
+          activedata.options.hidden=false;
+          // 显示节点时获取到被显示节点input，加入聚焦节点列表中
+          var addParent = this.$refs[activedata.model][0].$el;
+          addParent.classList.add("add");
+          var add = document.querySelector(".add input");
+          [...this.focusItems].splice(activeIndex,0,activeIndex+1)
+        }
+      }
+    },
+    // 监听表单数据改变
+    onInputChange(value, field) {
+      this.hideObj[field] = value;
+      let originData = this.data.list;
+      for(let i = 0; i < originData.length; i++){
+        this.hideData(originData[i],i)
+      }
+      // 向container组件发射on-change事件，将 key value 以及models（form表单的key value对象）传入
+      this.$emit("on-change", field, value, this.models);
+    },
+    // 获取全部可聚焦光标
+    getFocus(originData){
+        // 显示且非禁用元素
+        if(originData.options.hidden==false && !originData.options.disabled ) {
+          var parent = this.$refs[originData.model][0].$el;
+          parent.classList.add("canFocus")  //后期添加是否可聚焦类名
+        }
+    },
+    // 光标操作控制流程
+    handelFocus() {
+      if(this.focusIndex<this.focusItems.length){
+        this.focusItems[this.focusIndex].focus();
+        this.focusIndex++;
+      }
+    },
+    // 监听input组件发射的el-change事件
+    onElChange(e) {
+      this.getData().then(resolve => {
+        this.handelFocus()
+      });
+      this.$emit("input-enter");
+    }
+  },
+  watch: {
+    data: {
+      // 深度观察表单渲染对象，如果数据变更再次执行model生成函数
+      deep: true,
+      handler(val) {
+        this.generateModle(val.list);
+      }
+    },
+    value: {
+      // 深度观察组件key值
+      deep: true,
+      handler(val) {
+        console.log(JSON.stringify(val));
+        this.models = { ...this.models, ...val };
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+// @import '../styles/cover.scss';
+</style>
---
+++ src/components/Hr-input/index.js
@@ -0,0 +1,7 @@
+import HrInput from './HrInput';
+
+HrInput.install = function(Vue) {
+  Vue.component(HrInput.name, HrInput);
+};
+
+export default HrInput;
